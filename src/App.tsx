a9048d4aff8908d6474a726b31baa5db7213c34a
kalyntn97 <kalyntn@gmail.com>
2023-11-04 14:40:22 -0400

    add page transition animation to landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 //npm modules
 import { useContext, useState } from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import { AnimatePresence } from 'framer-motion'
 //components
 import NavBar from './components/NavBar/NavBar'
 import Footer from './components/Footer/Footer'
@@ -16,16 +17,20 @@ import styles from './App.module.scss'
 
 const App: React.FC = () => {
   const { theme } = useContext(ThemeContext)
+  const location = useLocation()
   const [showNavAndFooter, setShowNavAndFooter] = useState(true)
+
   return ( 
     <main className={styles.container} data-theme={theme}>
       {showNavAndFooter && <NavBar />}
-      <Routes>
-        <Route path='/' element={<Landing setShowNavAndFooter={setShowNavAndFooter} />} />
-        <Route path='/about' element={<About setShowNavAndFooter={setShowNavAndFooter}/>}/>
-        <Route path='/projects' element={<ProjectList setShowNavAndFooter={setShowNavAndFooter}/>}/>
-        <Route path='/contact' element={<Contact setShowNavAndFooter={setShowNavAndFooter}/>}/>
-      </Routes>
+      <AnimatePresence>
+        <Routes location={location} key={location.pathname}>
+          <Route path='/' element={<Landing setShowNavAndFooter={setShowNavAndFooter} />} />
+          <Route path='/about' element={<About setShowNavAndFooter={setShowNavAndFooter}/>}/>
+          <Route path='/projects' element={<ProjectList setShowNavAndFooter={setShowNavAndFooter}/>}/>
+          <Route path='/contact' element={<Contact setShowNavAndFooter={setShowNavAndFooter}/>}/>
+        </Routes>
+      </AnimatePresence>
       {showNavAndFooter && <Footer />}
     </main>
    )
--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,6 +1,7 @@
 //npm modules
 import { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
+import { motion } from 'framer-motion'
 //components
 import { ThemeContext } from '../../contexts/ThemeContext'
 import ThemeSwitcher from '../../components/ThemeSwitcher/ThemeSwitcher'
@@ -9,14 +10,52 @@ import styles from './Landing.module.scss'
 //assets
 import sleepingCat from '../../assets/images/sleepingCat.png'
 
-const Landing = (props) => {
+const Landing: React.FC = (props) => {
   props.setShowNavAndFooter(false)
   const { theme } = useContext(ThemeContext)
+  const variants = {
+    container: {
+      animate: { 
+        transition: { staggerChildren: 1 } 
+      },
+    },
+    laptop: {
+      initial: { y: -100, x: -750 },
+      animate: { y: 0 },
+      exit: { y: -100 },
+    },
+    image: { 
+      initial: { opacity: 0 },
+      animate: { opacity: 1 },
+      exit : { opacity: 0 }
+    },
+    windowBody : {
+      animate: {
+        transition: { delayChildren: 0.2, staggerChildren: 0.5 }
+      }
+    },
+    header : {
+      initial: { opacity: 0, x: -100 },
+      animate: { x: 0, opacity: 1 },
+      exit: { x: 100, opacity: 0 }
+    },
+    links : { 
+      initial: { x: -100, opacity: 0 },
+      animate: { x: 0, opacity: 1 },
+      exit: { x: 100, opacity: 0 }
+    }
+  }
+  
 
-  return ( 
-    <main className={styles.container} data-theme={theme}>
+  return (
+    <motion.div className={styles.container} data-theme={theme}
+      initial='initial'
+      animate='animate'
+      exit='exit'
+      variants={variants.container}
+    >
       <ThemeSwitcher />
-      <div className={styles.laptop}>
+      <motion.div variants={variants.laptop} key={'laptop'} className={styles.laptop}>
         <div className={styles.screen}>
           <div className={styles.window}>
             <div className={styles.windowBar}>
@@ -24,24 +63,24 @@ const Landing = (props) => {
               <div className={styles.roundBtn}></div>
               <div className={styles.roundBtn}></div>
             </div>
-            <div className={styles.windowBody}>
-              <div className={styles.header}>
+            <motion.div variants={variants.windowBody} key={'windowBody'} className={styles.windowBody}>
+              <motion.div variants={variants.header} key={'header'} className={styles.header}>
                 <h1>Tran Nguyen</h1>
                 <h2>a software engineer</h2>
-              </div>
-              <div className={styles.linkContainer}>
+              </motion.div>
+              <motion.div variants={variants.links} key={'links'} className={styles.linkContainer}>
                 <NavLink to='/about' data-replace='more about me'><span>more about me</span></NavLink>
                 <NavLink to='/projects' data-replace='my projects'><span>my projects</span></NavLink>
                 <NavLink to='/contact' data-replace='contact me'><span>contact me</span></NavLink>
-              </div>
-            </div>
+              </motion.div>
+            </motion.div>
           </div>
         </div>
         <div className={styles.keyboard}></div>
-      </div>
+      </motion.div>
       <div className={styles.table}></div>
-      <div className={styles.image}><img src={sleepingCat} alt="a sleeping cat" /></div>
-    </main>
+      <motion.div variants={variants.image} key={'image'} className={styles.image}><img src={sleepingCat} alt="a sleeping cat" /></motion.div>
+    </motion.div>
   )
 }
  
