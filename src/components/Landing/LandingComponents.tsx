7cbad2f95c1426dc8b3251c9f4907da2f312f7f7
kalyntn97 <tranhnnguyen13@gmail.com>
2024-06-02 08:54:23 -0400

    minor changes to css

--- src/components/Landing/LandingComponents.tsx
+++ src/components/Landing/LandingComponents.tsx
@@ -1,6 +1,8 @@
 import { NavLink } from "react-router-dom"
 import { navLinks } from "../../utils/ui"
 import styles from '../../pages/Landing/Landing.module.scss'
+//assets
+import resume from '/Tran_Nguyen_Resume.pdf'
 
 export const LinkSection = () => {
   return (
@@ -8,6 +10,7 @@ export const LinkSection = () => {
       {navLinks.map(link => 
         <NavLink key={link.name} to={link.href} data-replace={link.title} className={styles.slideLink}><span>{link.title}</span></NavLink>
       )}
+      {/* <a href={resume} target={'_blank'} className={styles.slideLink} data-replace='resume'><span>resume</span></a> */}
     </div>
   )
 }
